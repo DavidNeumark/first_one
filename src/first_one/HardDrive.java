c3ffa7f98362c074324cce1fe3096436bc4defb0
David NeumarK <d.neumark@gmail.com>
2020-11-18 17:32:03 +0200

    second exe

--- src/first_one/HardDrive.java
+++ src/first_one/HardDrive.java
@@ -8,7 +8,47 @@ public class HardDrive {
 	
 	public String solution(String S) {
 		
+		List<String> filesList = new ArrayList<String>(Arrays.asList(S.split("b\\s")));
+		System.out.println(filesList);
 		
+		int music = 0;
+		int image = 0;
+		int movie = 0;
+		int other = 0;
+		
+		for (String string : filesList) {
+			// music
+			if( string.matches(".*mp3 \\d*$") || string.matches(".*aac \\d*$") || string.matches(".*flac \\d*$")) {
+				string = string.replaceAll("(.*mp3 )?(\\D+)?","");
+				int memory = Integer.parseInt(string);
+				music = music + memory;
+				
+			// image
+			} else if( string.matches(".*jpg \\d*$") || string.matches(".*bmp \\d*$") || string.matches(".*gif \\d*$") ) {
+				string = string.replaceAll("\\D+","");
+				int memory = Integer.parseInt(string);
+				image = image + memory;
+				
+			// movie
+			} else if( string.matches(".*mp4 \\d*$") || string.matches(".*avi \\d*$") || string.matches(".*mkv \\d*$")) {
+				string = string.replaceAll("(.*mp4 )?(\\D+)?","");
+				int memory = Integer.parseInt(string);
+				movie = movie + memory;
+				
+			// other
+			} else if(string.matches(".* \\d*$") ) {
+				string = string.replaceAll(".*\\D+.*\\s","");
+				int memory = Integer.parseInt(string);
+				other = other + memory;
+				
+			}
+			
+			
+		}
+		System.out.println("music" + ": " + music);
+		System.out.println("image" + ": " + image);
+		System.out.println("movie" + ": " + movie);
+		System.out.println("other" + ": " + other);
 		
 		
 		return S;
@@ -17,25 +57,10 @@ public class HardDrive {
 	
 	public static void main(String[] args) {
 		
-		String s = "my.song.mp3 11b greatSong.flac 1000b not3.txt 5b video.mp4 200b game.exe 100b mov!e.mkv 10000b";
+		String S = "my.song.bmp 11b greatSong.flac 1000b not3.txt 5b video.mp4 200b game.exe 100b mov!e.mkv 10000b";
 		
-		List<String> filesList = new ArrayList<String>(Arrays.asList(s.split("b")));
-		System.out.println(filesList);
-		
-		
-		
-		for (String string : filesList) {
-			System.out.println(string.matches("\\.mp4 \\d*(,* )?"));
-			
-//			for(int i = 0; i < string.length(); i++) {
-//			   char c = string.charAt(i);
-//			   if( c == ' ') {
-//				   System.out.println(c);
-//			   }
-//			}
-			
-		}
-		
+		HardDrive list = new HardDrive();
+		list.solution(S);
 		
 	}
 
